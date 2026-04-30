ab3e25d357eda5b2b326967002ec5e700ec1d256
leandra <leandramarangoni8@gmail.com>
2026-04-30 01:49:31 +0000

    Telas Iniciais

---
+++ Control/src/routes/DrawerRoutes.tsx
@@ -0,0 +1,19 @@
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import HomeScreen from "../screens/HomeScreen";
+import EntidadesScreen from "../screens/EntidadesScreen";
+import ContasPagarScreen from "../screens/ContasPagarScreen";
+import ContasReceberScreen from "../screens/ContasReceberScreen";
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerRoutes() {
+     return (
+         <Drawer.Navigator>
+             <Drawer.Screen name="Home" component={HomeScreen} />
+              <Drawer.Screen name="Entidades" component={EntidadesScreen} />
+              <Drawer.Screen name="Contas a Pagar" component={ContasPagarScreen} />
+              <Drawer.Screen name="Contas a Receber" component={ContasReceberScreen} /> 
+         </Drawer.Navigator>
+     );
+}
